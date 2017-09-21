5a6e040b33446dae348f24a0a47954f0e53bcd4a
chris baus <christopher@baus.net>
2017-09-21 00:36:19 -0400

    add fixes for command-line emacs in MacOS. Don't load themes

--- init.el
+++ init.el
@@ -11,15 +11,17 @@
  ;; If there is more than one, they won't work right.
  )
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized/")
-'(add-to-list 'load-path "~/.emacs.d/less-css-mode/")
+
+(if (boundp 'custom-theme-load-path)
+   (progn (add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized/")
+  (load-theme 'solarized-dark t))
+  )
+
 (add-to-list 'load-path "~/.emacs.d/go-mode/")
-(load-theme 'solarized-dark t)
-'(require 'less-css-mode)
 (require 'go-mode-load)
 (desktop-save-mode 1)
 
-(global-linum-mode t)
+(setq global-linum-mode t)
 (setq gdb-many-windows t)
 (setq-default indent-tabs-mode nil)
 (setq create-lockfiles nil)
